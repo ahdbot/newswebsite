ec33dc0ec174bfbbe85edfe1ab7710b9cb27502c
ahdbot <ahdbot3@gmail.com>
2024-02-18 06:21:28 +0300

    page

--- js/index.js
+++ js/index.js
@@ -1,15 +1,28 @@
-var swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    autoplay: true,
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
+const mql = window.matchMedia("(max-width: 992px)");
 
+const changeSlideView = (slide) => {
+    var swiper = new Swiper(".swiper", {
+        slidesPerView: slide,
+        spaceBetween: 30,
+        autoplay: true,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+}
 
+const handleOrientationChange = (e) => {
+    if (mql.matches) {
+        changeSlideView(1);
+    } else {
+        changeSlideView(3);
+    }
+}
 
+handleOrientationChange();
 
-document.getElementById('year').innerHTML = new Date().getFullYear();
+mql.onchange = (e) => {
+    handleOrientationChange(e);
+}
